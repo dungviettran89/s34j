5e907fdb14abd7b2b8cb52b2eb9b088cab89ef31
dungviettran89 <dungviettran89@gmail.com>
2018-01-08 17:31:41 +0800

    Initial commit

---
+++ s34j-server/src/main/java/us/cuatoi/s34jserver/S34jServerApplication.java
@@ -0,0 +1,12 @@
+package us.cuatoi.s34jserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S34jServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S34jServerApplication.class, args);
+	}
+}
