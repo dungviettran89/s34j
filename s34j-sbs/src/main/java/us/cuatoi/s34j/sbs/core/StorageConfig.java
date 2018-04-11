8898a89b1ac035cd1f0658c8d919c2089402889f
Tran Viet Dung <dungviettran89@gmail.com>
2018-04-11 18:19:33 +0800

    Added base API for simple block service.

--- s34j-sbs/src/main/java/us/cuatoi/s34j/sbs/core/StorageConfig.java
+++ s34j-sbs/src/main/java/us/cuatoi/s34j/sbs/core/StorageConfig.java
@@ -1,9 +1,11 @@
 package us.cuatoi.s34j.sbs.core;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EntityScan("us.cuatoi.s34j.sbs.core")
+@ComponentScan("us.cuatoi.s34j.sbs.core")
 class StorageConfig {
 }
