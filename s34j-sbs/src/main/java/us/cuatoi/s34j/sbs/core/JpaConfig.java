d7820c8d84c8ede03faf8604d059544894ae886e
Tran Viet Dung <dungviettran89@gmail.com>
2018-04-11 15:46:55 +0800

    Added support for both mongodb and jpa

--- s34j-sbs/src/main/java/us/cuatoi/s34j/sbs/core/JpaConfig.java
+++ s34j-sbs/src/main/java/us/cuatoi/s34j/sbs/core/JpaConfig.java
@@ -1,7 +1,6 @@
 package us.cuatoi.s34j.sbs.core;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
