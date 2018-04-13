95cb32df0597b1483a9f631bb6b4a5646d53a3f0
Tran Viet Dung <dungviettran89@gmail.com>
2018-04-13 11:23:25 +0800

    Added new query to be used later in StoreSelector

--- s34j-sbs-server/src/test/java/us/cuatoi/s34j/sbs/server/StoreInformationUpdaterTest.java
+++ s34j-sbs-server/src/test/java/us/cuatoi/s34j/sbs/server/StoreInformationUpdaterTest.java
@@ -6,12 +6,15 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringRunner;
 import us.cuatoi.s34j.sbs.core.operation.StoreInformationUpdater;
 import us.cuatoi.s34j.sbs.core.store.model.ConfigurationModel;
 import us.cuatoi.s34j.sbs.core.store.model.ConfigurationRepository;
 import us.cuatoi.s34j.sbs.core.store.model.InformationModel;
 import us.cuatoi.s34j.sbs.core.store.model.InformationRepository;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
@@ -29,12 +32,26 @@ public class StoreInformationUpdaterTest {
 
     @Test
     public void testUpdateStorageInformation() {
+        InformationModel information = new InformationModel();
+        information.setName("unknown-information");
+        information.setActive(true);
+        information.setTotalBytes(2);
+        information.setAvailableBytes(1);
+        information.setUsedBytes(1);
+        informationRepository.save(information);
         storeInformationUpdater.perform();
         for (ConfigurationModel config : configurationRepository.findAll()) {
             InformationModel info = informationRepository.findOne(config.getName());
             logger.info("testUpdateStorageInformation() info=" + info);
             assertNotNull(info);
             assertEquals(info.getName(), config.getName());
+        }
+
+        List<InformationModel> sortedInformation = informationRepository.findByActiveOrderByAvailableBytesDesc(true, new PageRequest(0, 2));
+        for (InformationModel sorted : sortedInformation) {
+            logger.info("testUpdateStorageInformation() sorted=" + sorted);
+            assertNotNull(sorted);
+
         }
     }
 }
--- s34j-sbs/src/main/java/us/cuatoi/s34j/sbs/core/store/model/InformationRepository.java
+++ s34j-sbs/src/main/java/us/cuatoi/s34j/sbs/core/store/model/InformationRepository.java
@@ -1,6 +1,10 @@
 package us.cuatoi.s34j.sbs.core.store.model;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface InformationRepository extends CrudRepository<InformationModel, String> {
+    List<InformationModel> findByActiveOrderByAvailableBytesDesc(boolean active, Pageable pageable);
 }
