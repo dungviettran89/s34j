9edcc5ad4eaf331c8e0d1bdbe5ab57f4ed79be38
Tran Viet Dung <dungviettran89@gmail.com>
2018-05-11 22:28:48 +0800

    Added code to indicate query param verifier is no implemented.

--- s34j-spring/src/main/java/us/cuatoi/s34j/spring/auth/QueryParamsVerifier.java
+++ s34j-spring/src/main/java/us/cuatoi/s34j/spring/auth/QueryParamsVerifier.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright (C) 2018 dungviettran89@gmail.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ */
+
 package us.cuatoi.s34j.spring.auth;
 
-import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.jeasy.rules.annotation.*;
 import org.jeasy.rules.api.Facts;
 import org.springframework.stereotype.Service;
+import us.cuatoi.s34j.spring.SpringStorageException;
+import us.cuatoi.s34j.spring.dto.ErrorCode;
 
 @Service
 @Rule(name = "QueryParamsVerifier")
@@ -16,10 +32,9 @@ public class QueryParamsVerifier implements AuthenticationRule {
 
     @Condition
     public boolean shouldVerify(
-            @Fact("request") HttpServletRequest request,
             @Fact("query:X-Amz-Signature") String signature
     ) {
-        return false;
+        throw new SpringStorageException(ErrorCode.NOT_IMPLEMENTED);
     }
 
     @Action
